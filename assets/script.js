5f7e7b77061086f2e0359b5d5ebda73dd07b26af
vincent vissers <huskyix@MacBook-Pro-de-vincent.local>
2019-11-12 12:02:16 +0100

    push code

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
 //Use Get 
+//Request
+let xhttp = new XMLHttpRequest();
+xhttp.open('GET','https://thatsthespir.it/api',true);
+xhttp.send(null);
+//load
+xhttp.addEventListener("load", transferCompetejs,false);
+function transferCompetejs(evt){
+    let objetJson = JSON.parse(this.response);
+    const author = objetJson.author;
+    const quote = objetJson.quote;
+    const pic = objetJson.photo;
+    document.getElementById("quote").innerHTML = quote;
+    document.getElementById("author").innerHTML = author;
+    document.getElementById("picAuthor").src = pic;
+}
